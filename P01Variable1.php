1d577e46bfb45340e43af63c4a09b9127f52ba9b
OsmarBros <107452082+OsmarBros@users.noreply.github.com>
2024-10-21 09:09:50 -0600

    Add files via upload
    xdc

---
+++ P01Variable1.php
@@ -0,0 +1,24 @@
+<?php
+/*CBTIS89
+Programa que imprime el dìa, sueldo y nombre
+Luna Roacho Osmar Farid
+3a Programaciòn Matutino
+*/
+$dia = 24; //Se declara una variable tipo Integer.
+$sueldo = 758.43; //Se declara una variable tipo Double.
+$nombre = "PePito"; //Se declara una variable String.
+$exite = true; //Se declara una variable boolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable String:";
+echo $nombre; echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. "    ".$cadena2;
+?>
